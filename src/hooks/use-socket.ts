0e7689cd2184c3f684ae669db8da2a887ff2d8df
Anton <96301510+AntonOnoprienko@users.noreply.github.com>
2025-09-29 13:26:49 +0300

    Changed soket

--- src/hooks/use-socket.ts
+++ src/hooks/use-socket.ts
@@ -3,12 +3,13 @@ import { useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 
 let socket: Socket | null = null;
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:4000';
 
 export const useSocket = () => {
   const [activeSessions, setActiveSessions] = useState(0);
 
   useEffect(() => {
-    socket = io('http://localhost:4000');
+    socket = io(SOCKET_URL);
 
     socket.on('updateSessions', (count: number) => setActiveSessions(count));
 
